dd968ffa4fea993befd821a4da25c89772547333
Venkatesh Vanjaku <57989200+vvrofficial@users.noreply.github.com>
2020-02-15 12:46:58 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#Printing Dictionaries
+
+Info = {"Name": "Venkatesh Vanjaku", "Age": "22", "Height": "5.4", "Weight": "65"}
+
+print("Before Changing Age")
+
+print(Info)
+
+Info["Height"] = 5.9
+
+print("After Changing Age")
+
+print(Info)
