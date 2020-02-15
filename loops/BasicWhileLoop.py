a47fe58b28506cf1cfa19746b916a280322b69e4
Venkatesh Vanjaku <57989200+vvrofficial@users.noreply.github.com>
2020-02-15 18:21:30 +0530

    Update BasicWhileLoop.py

--- loops/BasicWhileLoop.py
+++ loops/BasicWhileLoop.py
@@ -1,3 +1,10 @@
+"""
+Task:
+1) Create program using while loop to collect student's names.
+2) Create a list and append student names to that list
+3) Exit from the loop if the user enters exit
+4. Print the Students List
+"""
 Students = []
 y = input("Enter student's name: ")
 while (y != "exit"):
