cb13896eab0292e8352b6c76b18d6fe140089f16
Venkatesh Vanjaku <57989200+vvrofficial@users.noreply.github.com>
2020-02-15 14:12:12 +0530

    Create stringmanipulation.py

---
+++ stringmanipulation.py
@@ -0,0 +1,10 @@
+print("########### String Manipulation in Python ###########")
+
+FirstName = input("Enter your first name: ")
+
+LastName = input("Enter your last name: ")
+
+print("Given Name: %s %s"%(FirstName, LastName))
+
+#We are printing only first letter in first name and last name.
+print("Given Name after String Manipulation: %s %s"%(FirstName[0], LastName[0]))
