15fd1ca004783ed6368bc09a46d8b592cc7e9b69
Venkatesh Vanjaku <57989200+vvrofficial@users.noreply.github.com>
2020-02-22 11:32:38 +0530

    Bulk Downloader
    A script to download files in bulk from a specific URL

---
+++ BulkDownloader.py
@@ -0,0 +1,24 @@
+#Downloading Images from a Link
+#Note:Make sure that you have downloaded required modules.
+
+import requests
+import wget
+from bs4 import BeautifulSoup
+
+#The URL from where we are going to download images
+url = "https://www.slideshare.net/secret/Ja6M4oWhJeik7O"
+
+#Response Type
+response = requests.get(url)
+
+#Parsing Response Using Beautiful Soup
+bs = BeautifulSoup(response.text, "html.parser")
+
+#Declaring what to download through classes and file type
+slides = bs.find_all("img",{"class":"slide_image"})
+
+#Downloading Slides and Storing with a Specific name
+for index,slide in enumerate(slides):
+    imagename = "Venky" + "-" + str(index) + ".jpg"
+    image = wget.download(slide["data-full"],imagename)
+    print("Downloading - " + slide["data-full"])
