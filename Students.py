adba737b07727640468fc17164509ab8b7d84907
Venkatesh Vanjaku <57989200+vvrofficial@users.noreply.github.com>
2020-02-22 10:22:06 +0530

    Update Students.py

--- Students.py
+++ Students.py
@@ -4,13 +4,23 @@
 3. Append One by One to the Students List
 4. Print Students List
 """
+
+#Declaring Blank Students List
 Students = []
+
+#Collecting Students Namesto Different Variables
 FirstStudent = input("Enter first student's first name: ")
 SecondStudent = input("Enter second student's first name: ")
 ThirdStudent = input("Enter third student's first name: ")
 FourthStudent = input("Enter fourth student's first name: ")
+
+#Appending Allt the Collected Names to Students List.
 Students.append(FirstStudent)
 Students.append(SecondStudent)
 Students.append(ThirdStudent)
 Students.append(FourthStudent)
+
+#Printing the Students List
 print (Students)
+
+print ("Credits: Venkatesh Vanjaku")
